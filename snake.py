e49cc02cbdbc49023ca98cd2489eaf80a2725216
shavitnoa <96386484+shavitnoa@users.noreply.github.com>
2021-12-19 21:17:29 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -8,35 +8,38 @@ class Snake:
         self.__head = self.__tail = None
         self.__length = 0
         self.__color = None
-        self.orientation = None
+        self.__orientation = "Right"
+        self.__locations = []
 
-    def add_new_head(self, new_head: Node):
+    def add_new_head(self, new_head: Node) -> None:
         """מתודה שמוסיפה חוליה לראש הנחש ומתעדכנת להיות הראש החדש
-       אם הנחש עוד לא הוגדר אז כעת הנחש יוגדר באורך 1 וראשו יהיה החוליה שבקלט"""
+               אם הנחש עוד לא הוגדר אז כעת הנחש יוגדר באורך 1 וראשו יהיה החוליה שבקלט"""
 
-        if self.__head is None:
-            # list was empty - snake didn't build yet
-            self.__tail = new_head
-        else:  # connect old head to new head
-            self.__head.prev = new_head
-            new_head.next = self.__head
-
-        # update head
-        self.__head = new_head
         self.__length += 1
+        if self.__head is None:
+            self.__head = new_head
+        else:
+            new_head.set_prev(self.__head)
+            self.__head.set_next(new_head)
+            self.__head = self.__head.get_next()
+        self.__locations.append(self.__head.get_data())
+
+    def get_location(self):
+        return self.__locations
 
     def get_head(self) -> Node:
         return self.__head
 
     def remove_tail(self):
-        """מתודה שמוחקת את החוליה האחרונה של הנחש ומתעדכנת באורך בהתאם"""
-        self.__tail = self.__tail.prev
-        if self.__tail is None:  # list is now empty
-            self.__head = None
-        else:  # disconnect old tail
-            self.__tail.next.prev = None
-            self.__tail.next = None
         self.__length -= 1
+
+        if self.__tail is None:
+             print('there is someting wrong, the snake is None')
+        else:
+            self.__tail = self.__tail.get_next()
+            self.__tail.get_prev().set_next(None)
+            self.__tail.set_prev(None)
+
 
     def in_crash(self, cell):
         """מתודה המקבלת מיקום של תא(x,y)
@@ -56,43 +59,51 @@ class Snake:
 
     def possible_moves(self):
         """מחזירה על סמך הכיוון הנוכחי של הנחש מהם המהלכים האפשריים"""
-        if self.orientation == "Right":
+        if self.__orientation == "Right":
             return ["Down", "Up"]
-        if self.orientation == "Left":
+        if self.__orientation == "Left":
             return ["Down", "Up"]
-        if self.orientation == "Up":
+        if self.__orientation == "Up":
             return ["Right", "Left"]
-        if self.orientation == "Down":
+        if self.__orientation == "Down":
             return ["Right", "Left"]
 
-    def move(self, movekey):
+    def set_orientation(self, movekey):
+        possible_move = self.possible_moves()
+        if movekey in possible_move:  # אם הכיוון נמצא ברשימת הכיוונים - נעדכן את הכיוון.
+            self.__orientation = movekey
+
+    def move(self):
         """פונקציה המקבלת כיוון ומחזירה אמת אם הנחש זז בהצלחה, שקר אחרת"""
-        if movekey in self.possible_moves():  # אם הכיוון נמצא ברשימת הכיוונים - נעדכן את הכיוון.
-            self.orientation = movekey        # אם לא, נמשיך עם הכיוון הנוכחי
         row_head = self.get_head().get_data()[0]
         col_head = self.get_head().get_data()[1]
-        if self.orientation == "Right":
+
+        if self.__orientation == "Right":
             x = row_head
             y = col_head + 1
-        if self.orientation == "Left":
+        elif self.__orientation == "Left":
             x = row_head
             y = col_head - 1
-        if self.orientation == "Down":
+        elif self.__orientation == "Down":
             x = row_head + 1
             y = col_head
-        if self.orientation == "Up":
+        else:
             x = row_head - 1
             y = col_head
         if self.in_crash((x, y)):  # אם הראש העתידי של הנחש יפגע בגופו נחזיר False
-            return False
+             return False
         new_head = Node((x, y))
         self.add_new_head(new_head)
+        print(self.get_location())
         self.remove_tail()
+        print(self.get_location())
         return True
-s = Snake()
-s.orientation = "Up"
-head = Node((7,9))
-s.add_new_head(head)
-print(s.get_head().get_data())
-s.move("Right")
-print(s.get_head().get_data())
+
+
+# s = Snake()
+# s.set_orientation = "Up"
+# head = Node((7,9))
+# s.add_new_head(head)
+# print(s.get_head().get_data())
+# s.move("Right")
+# print(s.get_head().get_data())
