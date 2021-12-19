757eba288aa5d69e6eedc21c44dd98beef284231
shavitnoa <96386484+shavitnoa@users.noreply.github.com>
2021-12-19 22:20:24 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,15 +1,14 @@
-import node
 from node import Node
-from typing import *
-
 
 class Snake:
     def __init__(self):
-        self.__head = self.__tail = None
+        self.__head: Node = None
+        self.__tail: None
         self.__length = 0
         self.__color = None
-        self.__orientation = "Right"
+        self.__orientation = 'Right'
         self.__locations = []
+
 
     def add_new_head(self, new_head: Node) -> None:
         """מתודה שמוסיפה חוליה לראש הנחש ומתעדכנת להיות הראש החדש
@@ -18,11 +17,16 @@ class Snake:
         self.__length += 1
         if self.__head is None:
             self.__head = new_head
+            self.__tail = new_head
         else:
             new_head.set_prev(self.__head)
             self.__head.set_next(new_head)
             self.__head = self.__head.get_next()
         self.__locations.append(self.__head.get_data())
+
+    # def add_tail(self, new_head: Node) -> None:
+    #     self.__length += 1
+    #     self.__tail = 5
 
     def get_location(self):
         return self.__locations
@@ -30,29 +34,14 @@ class Snake:
     def get_head(self) -> Node:
         return self.__head
 
+    def get_tail(self) -> Node:
+        return self.__tail
+
     def remove_tail(self):
         self.__length -= 1
-
-        if self.__tail is None:
-             print('there is someting wrong, the snake is None')
-        else:
-            self.__tail = self.__tail.get_next()
-            self.__tail.get_prev().set_next(None)
-            self.__tail.set_prev(None)
-
-
-    def in_crash(self, cell):
-        """מתודה המקבלת מיקום של תא(x,y)
-        מחזירה True אם התא הוא אחד מחוליות הנחש, False אחרת"""
-        return self.crash_helper(self.__head, cell, 0)
-
-    def crash_helper(self, cur, cell, index):
-        # שימוש פנימי: פונקציית עזר ל in_crash
-        if index >= self.__length:
-            return False
-        if cur.get_data() == cell:
-            return True
-        return self.crash_helper(cur.get_next(), cell, index + 1)
+        self.__tail = self.__tail.get_next()
+        self.__tail.get_prev().set_next(None)
+        self.__tail.set_prev(None)
 
     def set_color(self, color: str):
         self.__color = color
@@ -68,6 +57,7 @@ class Snake:
         if self.__orientation == "Down":
             return ["Right", "Left"]
 
+
     def set_orientation(self, movekey):
         possible_move = self.possible_moves()
         if movekey in possible_move:  # אם הכיוון נמצא ברשימת הכיוונים - נעדכן את הכיוון.
@@ -75,8 +65,8 @@ class Snake:
 
     def move(self):
         """פונקציה המקבלת כיוון ומחזירה אמת אם הנחש זז בהצלחה, שקר אחרת"""
-        row_head = self.get_head().get_data()[0]
-        col_head = self.get_head().get_data()[1]
+        row_head = self.get_head()[0]
+        col_head = self.get_head()[1]
 
         if self.__orientation == "Right":
             x = row_head
@@ -90,20 +80,9 @@ class Snake:
         else:
             x = row_head - 1
             y = col_head
-        if self.in_crash((x, y)):  # אם הראש העתידי של הנחש יפגע בגופו נחזיר False
-             return False
-        new_head = Node((x, y))
+        # if self.in_crash((x, y)):  # אם הראש העתידי של הנחש יפגע בגופו נחזיר False
+        #      return False
+        new_head = (x, y)
         self.add_new_head(new_head)
-        print(self.get_location())
         self.remove_tail()
-        print(self.get_location())
         return True
-
-
-# s = Snake()
-# s.set_orientation = "Up"
-# head = Node((7,9))
-# s.add_new_head(head)
-# print(s.get_head().get_data())
-# s.move("Right")
-# print(s.get_head().get_data())
