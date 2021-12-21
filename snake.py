1cba09a144559cabd6b87731282fe7bba2bae8a7
shavitnoa <96386484+shavitnoa@users.noreply.github.com>
2021-12-21 04:06:15 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,47 +1,44 @@
 from node import Node
+from typing import *
 
 class Snake:
     def __init__(self):
-        self.__head: Node = None
-        self.__tail: None
+        self.__head = None
+        self.__tail = None
         self.__length = 0
         self.__color = None
         self.__orientation = 'Right'
         self.__locations = []
 
-
-    def add_new_head(self, new_head: Node) -> None:
-        """מתודה שמוסיפה חוליה לראש הנחש ומתעדכנת להיות הראש החדש
-               אם הנחש עוד לא הוגדר אז כעת הנחש יוגדר באורך 1 וראשו יהיה החוליה שבקלט"""
-
+    def add_new_head(self, new_head: Tuple[int, int]) -> None:
+        """
+        adds a location to the snake's head
+        :param new_head: tuple of new head location
+        :return: None
+        """
         self.__length += 1
-        if self.__head is None:
-            self.__head = new_head
-            self.__tail = new_head
-        else:
-            new_head.set_prev(self.__head)
-            self.__head.set_next(new_head)
-            self.__head = self.__head.get_next()
-        self.__locations.append(self.__head.get_data())
-
-    # def add_tail(self, new_head: Node) -> None:
-    #     self.__length += 1
-    #     self.__tail = 5
+        self.__locations.insert(0,new_head) #TODO add to the begining
 
     def get_location(self):
         return self.__locations
 
-    def get_head(self) -> Node:
-        return self.__head
+    def get_head(self) -> Tuple[int, int]:
+        return self.__locations[0]
 
-    def get_tail(self) -> Node:
-        return self.__tail
+    def get_tail(self) -> Tuple[int, int]:
+        return self.__locations[-1]
+
+    def add_to_tail(self, new_tail: Tuple[int, int]) -> None:
+        self.__length += 1
+        self.__locations.append(new_tail)
 
     def remove_tail(self):
+        """
+        rmove the location of the snake's tail
+        :return: None
+        """
         self.__length -= 1
-        self.__tail = self.__tail.get_next()
-        self.__tail.get_prev().set_next(None)
-        self.__tail.set_prev(None)
+        self.__locations = self.get_location()[:-1]
 
     def set_color(self, color: str):
         self.__color = color
@@ -57,7 +54,6 @@ class Snake:
         if self.__orientation == "Down":
             return ["Right", "Left"]
 
-
     def set_orientation(self, movekey):
         possible_move = self.possible_moves()
         if movekey in possible_move:  # אם הכיוון נמצא ברשימת הכיוונים - נעדכן את הכיוון.
@@ -68,21 +64,18 @@ class Snake:
         row_head = self.get_head()[0]
         col_head = self.get_head()[1]
 
-        if self.__orientation == "Right":
+        if self.__orientation == "Up":
             x = row_head
             y = col_head + 1
-        elif self.__orientation == "Left":
+        elif self.__orientation == "Down":
             x = row_head
             y = col_head - 1
-        elif self.__orientation == "Down":
+        elif self.__orientation == "Right":
             x = row_head + 1
             y = col_head
         else:
             x = row_head - 1
             y = col_head
-        # if self.in_crash((x, y)):  # אם הראש העתידי של הנחש יפגע בגופו נחזיר False
-        #      return False
         new_head = (x, y)
         self.add_new_head(new_head)
-        self.remove_tail()
-        return True
+        return self.get_location().pop()
